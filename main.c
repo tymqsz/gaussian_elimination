9aa916795046624c9beb19da69e037ac22bfc1c0
Tymon Piwowarski <topiwowarski@gmail.com>
2024-01-02 17:24:59 +0100

    adding extracted trunk files

--- main.c
+++ main.c
@@ -1,18 +1,34 @@
-#include<iostream>
-#include<stdio.h>
+#include "gauss.h"
+#include "backsubst.h"
+#include "mat_io.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(){
-	int n, m;
-	
-	/* allocating memory for equations */
-	int** A = malloc(sizeof(int*) * n);
-	for(int i = 0; i < n; i++){
-		A[i] = malloc(sizeof(int) * m);
+
+int main(int argc, char ** argv) {
+	int res;
+	Matrix * A = readFromFile(argv[1]);
+	Matrix * b = readFromFile(argv[2]);
+	Matrix * x;
+
+	if (A == NULL) return -1;
+	if (b == NULL) return -2;
+	printToScreen(A);
+	printToScreen(b);
+
+	res = eliminate(A,b);
+	x = createMatrix(b->r, 1);
+	if (x != NULL) {
+		res = backsubst(x,A,b);
+
+		printToScreen(x);
+	  freeMatrix(x);
+	} else {
+					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
 	}
 
-	int* x = malloc(sizeof(int) * n);
-	int* b = malloc(sizeof(int) * n);
+	freeMatrix(A);
+	freeMatrix(b);
 
-	
+	return 0;
 }
-
